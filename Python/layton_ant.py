b6f5bc92511168711d221635977f70d074305119
KeiEl <maikhanh.le@student.adelaide.edu.au>
2014-11-04 15:39:35 +1030

    Change order of bin/python declaration in layton_ant

--- Python/layton_ant.py
+++ Python/layton_ant.py
@@ -1,25 +1,60 @@
+# 20
+# 0 1 1
+# 0 4 1
+# 1 2 1
+# 1 5 1
+# 2 3 2
+# 2 6 1
+# 3 8 1
+# 4 5 1
+# 4 11 2
+# 5 9 1
+# 6 7 1
+# 7 8 1
+# 7 10 1
+# 8 15 2
+# 9 10 2
+# 9 12 1
+# 10 14 1
+# 11 12 1
+# 11 16 1
+# 12 17 1
+# 13 14 1
+# 13 18 1
+# 14 15 1
+# 15 19 1
+# 16 17 1
+# 17 18 1
+# 18 19 2
+# 0 19
+# ^^^ Because I'm too stingy. Lol.
+
 #!/bin/python
 
 import sys
 
 def generate(filename):
 	f = open(filename,'r')
-	s = f.readline()
-	sz = int(s)
-	al = dict()
-	for i in range(0,sz):
-		al[i] = []
+
+	s = f.readline() # First line of actual input
+	sz = int(s.split(' ')[1])
+
+	al = [ [] for i in range(0,sz) ]
+
 	while True:
-		s = f.readline().split(' ')
-		if len(s) < 3:
+		s = f.readline().split(' ') # # n1 n2 w
+		if len(s) < 4:
 			break
-		al[int(s[0])].append( ( int(s[1]), int(s[2]) ) )
-		al[int(s[1])].append( ( int(s[0]), int(s[2]) ) )
-	src, dst = int(s[0]), int(s[1])
+		al[int(s[1])].append( ( int(s[2]), int(s[3]) ) )
+		al[int(s[2])].append( ( int(s[1]), int(s[3]) ) )
+
+	src, dst = int(s[1]), int(s[2]) # # src dst
 	f.close()
+
 	return al, src, dst
 
 def dfs( g, src, dst, h, curr, curr_p ):
+	# Need to redo this... too clunky!
 	if src == dst:
 		return True, curr, [dst]
 	max_d = -1
@@ -43,9 +78,7 @@ def max_path( g, src, dst ):
 	return dfs( g, src, dst, h, 0, p )
 
 def main():
-	if len(sys.argv) < 2:
-		print "Usage", sys.argv[0], "[pathname]"
-	al, src, dst = generate( sys.argv[1] )
+	al, src, dst = generate( sys.argv[0] ) # Read itself for input
 	found, res, p = max_path( al, src, dst )
 	if not found:
 		print "No path found."
